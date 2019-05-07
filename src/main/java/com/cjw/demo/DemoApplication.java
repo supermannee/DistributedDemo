dbac6aaacc082848134bbb43092ac804cedf40b1
黄琦 <huangqi@huangqideMacBook-Pro.local>
2019-05-07 10:47:25 +0800

    Initial commit

---
+++ src/main/java/com/cjw/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.cjw.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+        System.out.println("-------程序启动成功-------");
+    }
+}
